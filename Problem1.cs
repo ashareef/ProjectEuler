9ccd85cfb027c9510506ca8a58898960046c0535
ashareef <aj.shareef@gmail.com>
2013-05-12 16:53:07 -0400

    problem 1 solved

---
+++ Problem1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectEuler
+{
+    public static class Problem1
+    {
+        public static long Solution(long limit)
+        {
+            long sum = 0;
+            for (long i = 0; i < limit; i++) {
+                if ((i % 3 == 0) || (i % 5 == 0)) {
+                    sum += i;
+                }
+            }
+            return sum;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,7 +11,14 @@ namespace ProjectEuler
     {
         static void Main(string[] args)
         {
-            Debug.WriteLine("test");
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+
+            long answer = Problem1.Solution(1000);
+            Debug.WriteLine(answer);
+
+            sw.Stop();
+            Debug.WriteLine("elapsed: {0}", sw.Elapsed);
         }
     }
 }
