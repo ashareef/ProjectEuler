b7796f142782626b9ec3d036ab4f6edbab08dadd
ashareef <aj.shareef@gmail.com>
2013-05-12 17:27:22 -0400

    problem 1 - O(1) solution

--- Problem1.cs
+++ Problem1.cs
@@ -11,11 +11,11 @@ namespace ProjectEuler
         public static long Solution(long limit)
         {
             long sum = 0;
-            for (long i = 0; i < limit; i++) {
-                if ((i % 3 == 0) || (i % 5 == 0)) {
-                    sum += i;
-                }
-            }
+            // sum of arithmetic progression
+            // n * (a_1 + a_n) / 2
+            sum += ((long)limit - 1) / 3 * (3 + 999) / 2;
+            sum += ((long)limit - 1) / 5 * (5 + 995) / 2;
+            sum -= ((long)limit - 1) / 15 * (15 + 990) / 2;
             return sum;
         }
     }
