dfc65f7cef187bb6d208d265eb0346601966638b
ashareef <aj.shareef@gmail.com>
2014-01-19 01:58:45 -0500

    f(4), f(10), f(157) solves but not for all

--- Problem455.cs
+++ Problem455.cs
@@ -8,39 +8,39 @@ using System.Numerics;
 
 namespace ProjectEuler
 {
-    public static class Problem455
-    {
-        public static void Solution()
-        {
-            BigInteger mLimit = 1000000000;
-            BigInteger limit = 4;
-            BigInteger sum = 0;
+	public static class Problem455
+	{
+		public static void Solution()
+		{
+			BigInteger mLimit = 1000000000;
+			BigInteger limit = 4;
+			BigInteger sum = 0;
 
-            for (BigInteger n = limit; n <= limit; n++)
-            {
-                BigInteger m = 10;
-                BigInteger k = 1;
-                BigInteger inc = 1;
-                BigInteger kMax = k;
-                while (m < mLimit)
-                {                 
-                    if (BigInteger.ModPow(n, k, m) == k)
-                    {
-                        inc *= 10;
-                        m *= 10;
-                        kMax = k;
-                        k += inc;
-                    }
-                    else
-                    {
-                        k += inc;
-                    }
-                }
-                Debug.WriteLine(kMax);
-            }
-   
-        }
+			for (BigInteger n = limit; n <= limit; n++)
+			{
+				BigInteger k = 0;
+				BigInteger inc = 1;
+				BigInteger kMax = 0;
+				BigInteger m = 10;
+				for (BigInteger i = 1; i < (BigInteger) Math.Pow(10, n.ToString().Length); i++)
+				{
+					if (BigInteger.ModPow(n, i, m) == i)
+					{
+						m *= 10;
+						k = BigInteger.ModPow(n, i, m);
+						while (m < mLimit)
+						{
+							m *= 10;
+							k = BigInteger.ModPow(n, k, m);
+							//Debug.WriteLine(k + " " + m);
+						}
+					}
+				}
+				sum += k;
+			}
+			Debug.WriteLine(sum);
+		}
+		
 
-
-    }
+	}
 }
