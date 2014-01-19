32fc45085fe61a382e3db63f5480aee16cfca3d1
ashareef <aj.shareef@gmail.com>
2014-01-19 04:25:23 -0500

    faster by adding break

--- Problem455.cs
+++ Problem455.cs
@@ -13,16 +13,15 @@ namespace ProjectEuler
 		public static void Solution()
 		{
 			BigInteger mLimit = 1000000000;
-			BigInteger limit = 4;
+			BigInteger limit = 99;
 			BigInteger sum = 0;
 
 			for (BigInteger n = limit; n <= limit; n++)
 			{
 				BigInteger k = 0;
 				BigInteger inc = 1;
-				BigInteger kMax = 0;
 				BigInteger m = 10;
-				for (BigInteger i = 1; i < (BigInteger) Math.Pow(10, n.ToString().Length); i++)
+                for (BigInteger i = (BigInteger) Math.Pow(10, n.ToString().Length); i > 0; i--)
 				{
 					if (BigInteger.ModPow(n, i, m) == i)
 					{
@@ -32,10 +31,13 @@ namespace ProjectEuler
 						{
 							m *= 10;
 							k = BigInteger.ModPow(n, k, m);
-							//Debug.WriteLine(k + " " + m);
+							
 						}
+						break;
 					}
+				
 				}
+                //Debug.WriteLine(n + " " + k);
 				sum += k;
 			}
 			Debug.WriteLine(sum);
