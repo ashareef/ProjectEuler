0d3cfe768a0f20f2c18db02a271b45f365fe7726
ashareef <aj.shareef@gmail.com>
2014-01-19 20:22:47 -0500

    added sieve of eratosthenes + utility class

---
+++ PEUtil.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjectEuler
+{
+	/// <summary>
+	/// Commonly used functions in Project Euler
+	/// </summary>
+	public static class PEUtil
+	{
+		public static List<long> Primes(long n)
+        {
+            List<long> result = new List<long>();
+            bool[] notPrimes = new bool[n];
+
+            for (long i = 2; i < n; i++)
+                for (long j = i + i; j < n; j += i)
+                    notPrimes[j] = true;
+
+            for (long i = 2; i < n; i++)
+                if (notPrimes[i] == false)
+                    result.Add(i);
+            return result;
+        }
+	}
+}
--- Problem268.cs
+++ Problem268.cs
@@ -34,6 +34,21 @@ namespace ProjectEuler
 
 		}
 		
+		public static List<long> Primes(long n)
+        {
+            List<long> result = new List<long>();
+            bool[] notPrimes = new bool[n];
+
+            for (long i = 2; i < n; i++)
+                for (long j = i + i; j < n; j += i)
+                    notPrimes[j] = true;
+
+            for (long i = 2; i < n; i++)
+                if (notPrimes[i] == false)
+                    result.Add(i);
+            return result;
+        }
+		
 		
 	}
 }
