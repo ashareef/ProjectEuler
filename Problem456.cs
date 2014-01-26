21efdff8a1b3619491e893a838e026af0833267e
ashareef <aj.shareef@gmail.com>
2014-01-25 22:40:32 -0500

    naive solution for 496

--- Problem456.cs
+++ Problem456.cs
@@ -13,26 +13,37 @@ namespace ProjectEuler
 	{
 		public static void Solution()
 		{
-			int n = 8;
-			HashSet<Point> points = new HashSet<Point>();
+			int n = 600;
+			List<Point> p = new List<Point>();
+			Point o = new Point(0, 0);
+			HashSet<Point> h = new HashSet<Point>();
 			
 			for (int i = 1; i <= n; i++)
 			{
 				int x = (int) BigInteger.ModPow(1248, i, 32323) - 16161;
 				int y = (int) BigInteger.ModPow(8421, i, 30103) - 15051;
-				points.Add(new Point(x, y));
+				//p.Add(new Point(x, y));
+				h.Add(new Point(x, y));
 			}
 			
-			foreach (var p1 in points) {
-				foreach (var p2 in points) {
-					foreach (var p3 in points) {
-						if (!(p1.Equals(p2) || p2.Equals(p3) || p1.Equals(p3)))
-						{
-								
-						}
-					}
-				}
-			}
+			long count = 0;
+//			for (int i = 0; i < p.Count; i++) {
+//				for (int j = i + 1; j < p.Count; j++) {
+//					for (int k = j + 1; k < p.Count; k++) {
+//						if (T(p[i], p[j], p[k]) == T(p[i], p[j], o) + T(p[i], o, p[k]) + T(o, p[j], p[k])) {
+//							//count++;
+//						}
+//						count++;
+//					}
+//				}
+//			}
+			
+			Debug.WriteLine(count);
+		}
+		
+		public static double T(Point p1, Point p2, Point p3)
+		{
+			return Math.Abs((p1.X - p3.X) * (p2.Y - p1.Y) - (p1.X - p2.X) * (p3.Y - p1.Y));
 		}
 	}
 }
