b11b11375813283332aadd01bfb93690c87a1eaa
ashareef <aj.shareef@gmail.com>
2014-01-29 22:29:20 -0500

    P64 - Better method, terminates on period

--- Problem64.cs
+++ Problem64.cs
@@ -7,24 +7,34 @@ namespace ProjectEuler
 	public static class Problem64
 	{
 		// http://web.math.princeton.edu/mathlab/jr02fall/Periodicity/mariusjp.pdf
+		// http://en.wikipedia.org/wiki/Methods_of_computing_square_roots#Continued_fraction_expansion
 		
 		public static void Solution()
 		{
-			List<int> fraction = ContinuedFractionSqrt(13);
-			foreach (var d in fraction) {
-				Debug.WriteLine(d);
+			int limit = 13;
+			for (int n = 1; n <= limit; n++) {
+				List<int> fraction	
 			}
+				
 		}
 		
 		public static List<int> ContinuedFractionSqrt(int n)
 		{
 			List<int> continuedFraction = new List<int>();
-			double x = Math.Sqrt(n);
-			for (int i = 0; i < 20; i++) {
-				int a = (int) Math.Floor(x);
-				x = 1 / (x - a);
-				continuedFraction.Add(a);				
+			int m = 0;
+			int d = 1;
+			int a0 = (int) Math.Floor(Math.Sqrt(n));
+			int a = a0;
+			continuedFraction.Add(a);
+			
+			while (a != 2 * a0) {
+				m = d * a - m;
+				d = (n - m * m) / d;
+				a = (int) Math.Floor((double) (a0 + m) / d);
+				continuedFraction.Add(a);
 			}
+			
+			
 			return continuedFraction;
 		}
 	}
