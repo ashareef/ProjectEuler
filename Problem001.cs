baa33c65d310a355c64d7b88b2d91281872d4c89
ashareef <aj.shareef@gmail.com>
2014-02-13 11:54:46 -0500

    P1 - Solution

--- Problem001.cs
+++ Problem001.cs
@@ -1,22 +1,28 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjectEuler
 {
-    public static class Problem1
-    {
-        public static long Solution(long limit)
-        {
-            long sum = 0;
-            // sum of arithmetic progression
-            // n * (a_1 + a_n) / 2
-            sum += ((long)limit - 1) / 3 * (3 + 999) / 2;
-            sum += ((long)limit - 1) / 5 * (5 + 995) / 2;
-            sum -= ((long)limit - 1) / 15 * (15 + 990) / 2;
-            return sum;
-        }
-    }
+	/// <summary>
+	/// Multiples of 3 and 5
+	/// </summary>
+	/// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+	/// Find the sum of all the multiples of 3 or 5 below 1000.
+
+	public static class Problem001
+	{
+		public static void Solution()
+		{
+			long sum = 0;
+			long limit = 1000;
+			// sum of arithmetic progression
+			// n * (a_1 + a_n) / 2
+			sum += ((long)limit - 1) / 3 * (3 + 999) / 2;
+			sum += ((long)limit - 1) / 5 * (5 + 995) / 2;
+			sum -= ((long)limit - 1) / 15 * (15 + 990) / 2;
+			
+			Debug.WriteLine(sum);
+		}
+	}
 }
--- Program.cs
+++ Program.cs
@@ -14,7 +14,7 @@ namespace ProjectEuler
             Stopwatch sw = new Stopwatch();
             sw.Start();
 			
-            Problem003.Solution();
+            Problem001.Solution();
             
             sw.Stop();
             Debug.WriteLine("elapsed: {0}", sw.Elapsed);
