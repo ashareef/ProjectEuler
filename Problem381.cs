51d6051e13e3bbf3519bed15d13fe504181e2999
ashareef <aj.shareef@gmail.com>
2014-02-21 08:41:20 -0500

    P381 - Created S() + Naive Solution

--- Problem381.cs
+++ Problem381.cs
@@ -2,6 +2,9 @@
 // 2/21/2014
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Numerics;
 
 namespace ProjectEuler
 {
@@ -14,7 +17,7 @@ namespace ProjectEuler
 	/// (7-1)! + (7-2)! + (7-3)! + (7-4)! + (7-5)! = 6! + 5! + 4! + 3! + 2! = 720+120+24+6+2 = 872.
 	/// As 872 mod(7) = 4, S(7) = 4.
 	/// It can be verified that ∑S(p) = 480 for 5 ≤ p < 100.
-	/// Find ∑S(p) for 5 ≤ p < 108.
+	/// Find ∑S(p) for 5 ≤ p < 10^8.
 	#endregion
 	
 	public static class Problem381
@@ -22,11 +25,36 @@ namespace ProjectEuler
 		public static void Solution()
 		{
 			NaiveSolution();
+			Debug.WriteLine(S(7) % 7);
 		}
 		
-		public static void NaiveSolution
+		public static void NaiveSolution()
 		{
-			
+			List<long> primes = PEUtil.Primes((long) 100);
+			primes.RemoveAt(0); // remove 2
+			primes.RemoveAt(0); // remove 3
+			foreach (var p in primes) {
+				
+			}
 		}
+		
+		public static long S(long p)
+		{
+			long sum = 0;
+			for (int k = 1; k <= 5; k++) {
+				sum += Factorial(p - k);
+			}
+			return sum;
+		}
+		
+		public static long Factorial(long n)
+		{
+			long product = 1;
+			for (int i = 2; i <= n; i++) {
+				product *= i;
+			}
+			return product;
+		}
+		
 	}
 }
